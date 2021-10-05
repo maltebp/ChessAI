170555392e5a0ab190a21a64c2b54a796471b361
peterrevsbech <peter.revsbech@gmail.com>
2021-10-05 14:14:31 +0200

    Created simple heuristic summing values of pieces

--- src/MinMax.cpp
+++ src/MinMax.cpp
@@ -10,7 +10,7 @@ public:
 	std::tuple<Move ,int> search(State state, int depth, int alpha, int beta) {
 		//Base case: Leaf node
 		if (depth == 0) {
-			int score = randomHeuristic(state);
+			int score = simpleHeuristic(state);
 			return { Move(), score };
 		}
 
@@ -44,6 +44,42 @@ public:
 
 	int randomHeuristic(State state) {
 		return rand() % 100;
+	}
+
+	int simpleHeuristic(State state) {
+		int sum = 0;
+		for (int i = 0; i < 8; i++) {
+			for (int j = 0; j < 8; j++) {
+				Piece piece = state.board[i][j];
+				int sign = piece.getColor() == PieceColor::WHITE ? 1 : -1;
+				switch (piece.getType())
+				{
+				case PieceType::KING:
+					sum += sign * 1000;
+					break;
+				case PieceType::QUEEN:
+					sum += sign * 9;
+					break;
+				case PieceType::ROOK:
+					sum += sign * 5;
+					break;
+				case PieceType::BISHOP:
+					sum += sign * 3;
+					break;
+				case PieceType::KNIGHT:
+					sum += sign * 3;
+					break;
+				case PieceType::PAWN:
+					sum += sign * 1;
+					break;
+				case PieceType::NONE:
+				default:
+					break;
+				}
+			}
+		}
+
+		return sum;
 	}
 };
 
