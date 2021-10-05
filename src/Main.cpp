6f39b506e7706d260cf05ba343a2304c6e178c33
Malte B.P <maltebp94@gmail.com>
2021-10-05 17:27:29 +0200

    Added Catch2 and setup Unit Tests

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,6 +1,8 @@
+#ifndef UNIT_TEST
 
 #include "GameController.h"
 #include "TUIPlayer.h"
+
 
 int main(int argc, char** argv) {
 
@@ -12,3 +14,34 @@ int main(int argc, char** argv) {
 
     return 0;
 }
+
+
+
+#else
+
+#define CATCH_CONFIG_RUNNER
+#include "external/catch.hpp"
+
+
+// Test files --------
+
+#include "UnitTests/TestTest.h"
+
+// -------------------
+
+
+int main(int argc, const char** argv) {
+
+    std::vector<const char*> args;
+    for(int i=0; i<argc; i++) {
+        args.push_back(argv[i]);
+    }
+	
+	// Test arguments: (i.e. tag on test cases to run)
+	//args.push_back("-b"); // Break when assertion failed
+
+    return Catch::Session().run((int)args.size(), args.data());
+}
+
+
+#endif // UNIT_TEST
---
+++ src/UnitTests/TestTest.h
@@ -0,0 +1,10 @@
+#pragma once
+
+#include "external/catch.hpp"
+
+
+TEST_CASE("Unit Test Test", "[general]") {
+
+    REQUIRE(2 == 2);
+
+}
