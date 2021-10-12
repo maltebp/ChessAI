3cdc3aed005a517f9e5d3b53576ebd4f2c44cdc7
Malte B.P <maltebp94@gmail.com>
2021-10-12 11:35:40 +0200

    Added test for PawnMoves

--- src/UnitTests/PawnMoves.h
+++ src/UnitTests/PawnMoves.h
@@ -25,3 +25,31 @@ TEST_CASE("Pawn Moves: Start state", "[moves][pawn]") {
         REQUIRE(contains(moves, Move{ {i,6}, {i,4} }));
     }
 }
+
+
+TEST_CASE("En Passant", "[moves][pawn]") {
+
+    State state;
+    state.board[0][0] = { PieceColor::WHITE, PieceType::KING };
+    state.board[7][7] = { PieceColor::BLACK, PieceType::KING };
+    state.board[2][1] = { PieceColor::WHITE, PieceType::PAWN };
+    state.board[3][3] = { PieceColor::BLACK, PieceType::PAWN };
+    
+    REQUIRE(state.enPassantTarget == Position());
+
+    // Move white pawn two forward -> "enabling" en passant for black
+    Move moveWhitePawn = { {2,1}, {2,3} };
+    state = MoveUtil::executeMove(state, moveWhitePawn);
+
+    // Check that black can en passant
+    REQUIRE(state.enPassantTarget == Position{2,2});
+    std::vector<Move> blackMoves = MoveUtil::getAllMoves(state);
+    Move moveBlackPawn = Move{{3,3},{2,2}};
+    REQUIRE(contains(blackMoves, moveBlackPawn));
+
+    // Perform en passant
+    state = MoveUtil::executeMove(state, moveBlackPawn);
+    REQUIRE(state.board[2][3] == Piece());
+    REQUIRE(state.board[2][2] == Piece{PieceColor::BLACK, PieceType::PAWN});
+    REQUIRE(state.enPassantTarget == Position());
+}
