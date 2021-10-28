01e428f18d99e7d23e241af7658141167db8b465
Malte B.P <maltebp94@gmail.com>
2021-10-28 19:45:26 +0200

    Removed unit tests from Game/Main.cpp

--- Game/src/Main.cpp
+++ Game/src/Main.cpp
@@ -1,5 +1,3 @@
-#ifndef UNIT_TEST
-
 #include "GameController.h"
 #include "TUIPlayer.h"
 #include "AIPlayerController.h"
@@ -19,38 +17,3 @@ int main(int argc, char** argv) {
 
     return 0;
 }
-
-
-
-#else
-
-#define CATCH_CONFIG_RUNNER
-#include "external/catch.hpp"
-
-
-// Test files --------
-
-#include "UnitTests/PawnMoves.h"
-#include "UnitTests/CastlingMoves.h"
-#include "UnitTests/PromotionMoves.h"
-#include "UnitTests/FEN.h"
-#include "UnitTests/CheckMate.h"
-
-// -------------------
-
-
-int main(int argc, const char** argv) {
-
-    std::vector<const char*> args;
-    for(int i=0; i<argc; i++) {
-        args.push_back(argv[i]);
-    }
-	
-	// Test arguments: (i.e. tag on test cases to run)
-	args.push_back("-b"); // Break when assertion failed
-
-    return Catch::Session().run((int)args.size(), args.data());
-}
-
-
-#endif // UNIT_TEST
