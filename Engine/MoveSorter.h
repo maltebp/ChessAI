0ef3ba0db6881859f3255823300aac030dda675a
peterrevsbech <peter.revsbech@gmail.com>
2021-11-02 13:07:27 +0100

    fixed build error in MoveSorter

--- Engine/MoveSorter.h
+++ Engine/MoveSorter.h
@@ -11,13 +11,54 @@ class MoveSorter {
 	};
 
 public:
+
+
+	int getStaticPieceValue(PieceType type) {
+		switch (type) {
+		case PieceType::QUEEN:
+			return 900;
+			break;
+		case PieceType::KNIGHT:
+			return 300;
+			break;
+		case PieceType::BISHOP:
+			return 300;
+			break;
+		case PieceType::ROOK:
+			return 500;
+			break;
+		case PieceType::PAWN:
+			return 100;
+			break;
+		default:
+			break;
+		}
+	}
+
+	/**
+	bool: indicates if it is a capture mvoe
+	int: indicates relative "gain" calculated as agressor_value - victim_value
+	DOES NOT TAKE CARE OF EN PASSANT AND CASTLING MOVES
+	*/
+	std::tuple<bool, int> getMoveCaptureBenfit(const State& state, const Move& move) {
+		Piece agressor = state[move.fromField];
+		Piece victim = state[move.toField];
+		int benefit = 0;
+
+		bool isCapture = victim.getType() != PieceType::NONE;
+		if (isCapture) {
+			benefit = getStaticPieceValue(victim.getType()) - getStaticPieceValue(agressor.getType());
+		}
+		return { isCapture, benefit };
+	}
+
 	void sortMoves(const State& state, std::vector<Move>& moves) {
 		int lastCaptureIndex = 0;
 		std::array<element, 100> moveElements;
 		//Move capture- moves to front
 		for (int i = 0; i < moves.size(); i++) {
 			Move move = moves[i];
-			auto [isCaptureMove, benefit] = MoveUtil::getMoveCaptureBenfit(state, move);
+			auto [isCaptureMove, benefit] = getMoveCaptureBenfit(state, move);
 			moveElements[i] = { move, benefit};
 			if (isCaptureMove) {
 				struct element tmp = moveElements[lastCaptureIndex];
