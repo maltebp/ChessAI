5197cc6f7bcda6203666191729cdd98e7367094e
Malte B.P <maltebp94@gmail.com>
2021-11-16 11:17:26 +0100

    Transposition table: added mask conversion

--- Engine/src/Transposition.h
+++ Engine/src/Transposition.h
@@ -1,6 +1,26 @@
 #pragma once
 namespace Transposition {
-	const int size = 4194304;
+
+	const size_t INDEX_BITS = 22;
+
+	static constexpr unsigned long long getIndexMask() {
+		unsigned long long mask = 0;
+		unsigned long long setBit = 1;
+
+		for( size_t i = 0; i < INDEX_BITS; i++ ) {
+			mask |= setBit;
+			setBit = setBit << 1;
+		}
+		
+		return mask;
+	}
+
+	constexpr int size = getIndexMask() + 1;
+
+	static unsigned long long hashToIndex(unsigned long long hash) {
+		return hash & getIndexMask();
+	}
+
 	struct TranspositionEntry {
 		short entryCounter;
 		short score;
@@ -15,19 +35,20 @@ namespace Transposition {
 		}
 	}
 	static void insertEntry(unsigned long long hash, short score) {
-		table[hash].entryCounter = tableCounter;
-		table[hash].score = score;
+		table[hashToIndex(hash)].entryCounter = tableCounter;
+		table[hashToIndex(hash)].score = score;
 	}
 
 	static bool isEntry(unsigned long long hash) { // if it already exists for this counter it'll 
 		//return true. if it hasn't been explored yet it returns false.
-		if (table[hash].entryCounter == tableCounter) {
+		if (table[hashToIndex(hash)].entryCounter == tableCounter) {
 			return true;
 		}
 		return false;
 	}
+	
 	static short getScore(unsigned long long hash) {
-		return table[hash].score;
+		return table[hashToIndex(hash)].score;
 	}
 
 
