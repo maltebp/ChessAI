315ef71d23a8eb06dad429b643d45db66c03027f
Malte B.P <maltebp94@gmail.com>
2021-11-20 23:44:40 +0100

    Opening book: fixed crash at init

--- Arena/src/Main.cpp
+++ Arena/src/Main.cpp
@@ -22,7 +22,7 @@ int main(int argc, char* argv[]) {
     State state = State::createDefault();
 
     UCIEngine stockfish1 = UCIEngine("Stockfish", STOCKFISH_PATH, 9975);
-    AIPlayerController ourEngine = AIPlayerController(9975, false);
+    AIPlayerController ourEngine = AIPlayerController(9975, true);
 
     // UCIEngine stockfish1 = UCIEngine("Stockfish", STOCKFISH_PATH, 250);
     // AIPlayerController ourEngine = AIPlayerController(5);
