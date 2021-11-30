17173d676611192e13744cbd53002becbdff830c
Malte B.P <maltebp94@gmail.com>
2021-11-30 16:01:35 +0100

    Fixed book moves

--- Game/src/Main.cpp
+++ Game/src/Main.cpp
@@ -9,14 +9,8 @@ int main(int argc, char** argv) {
     TUIPlayer localPlayer1;
     TUIPlayer localPlayer2;
     AIPlayerController ourEngine1(9900, true);
-    //AIPlayerController ourEngine2(5000, true);
     
-    State startState;
-    startState.board[0][0] = { PieceColor::WHITE, PieceType::KING };
-    startState.board[7][7] = { PieceColor::BLACK, PieceType::KING };
-    startState.board[0][6] = { PieceColor::WHITE, PieceType::PAWN };
-
-    //State startState = State::createDefault();
+    State startState = State::createDefault();
 
     GameController game = GameController(localPlayer1, ourEngine1);
     game.clearScreen = false;
